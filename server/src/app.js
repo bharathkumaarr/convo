b80408d452beb87d4b3d7f11c8283c6f3376803e
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2026-01-18 02:58:07 +0530

    refined

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import cookieparser from 'cookie-parser'
+import authRoutes from "./routes/auth.routes.js";
+
 
 const app = express()
 
 
 app.use(express.json())
 app.use(cookieparser())
+
+app.use("/api/auth", authRoutes);
+
 
 
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv'
 import app from './app.js'
 
+import { connectDB } from "./utils/db.js";
+
+
 dotenv.config();
 
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, ()=>{
-    console.log(`convo server running live on ${PORT}`)
-})
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Convo api running on port ${PORT}`);
+  });
+};
+
+startServer();
